5a1e49b1fd7c4ac992d3b223c53ae66e35f3e6ca
Dan Carroll <dan@sputnik.net.au>
2018-10-30 03:36:24 +0000

    Setup script

--- flightline/files/setup.sh
+++ flightline/files/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ $(whoami) != "root" ]; then
+  echo "ERROR: This script must execute as the root user.   Pease call again using 'sudo'"
+  exit
+fi
+
+checkDir() {
+ dirToCheck = $1
+ if [ -e "$dirToCheck" && -d "$dirToCheck" ]; then
+   echo "INFO: Dir $dirToCheck is OK."
+ else
+   echo "ERROR: Dir $dirToCheck is NOT OK.  Aborting."
+   exit
+ fi
+}
+
 [ ! -e /data ] && mkdir /data
 [ ! -e /data/volumes/web ] && mkdir -p /data/volumes/web
 [ ! -e /data/volumes/html ] && mkdir -p /data/volumes/html
@@ -9,4 +24,10 @@ checkDir /data/volumes
 checkDir /data/volumes/web
 checkDir /data/volumes/html
 
+if [ -e /data/composer/flightline/files/default.conf ]; then
+  echo "INFO: copying web server config"
+  cp /data/composer/flightline/files/default.conf /data/volumes/web/
+fi
 
+echo "INFO: cloning flightline web app."
+git clone https://danny@git.dannysplace.net/scm/score/score-flightline-node.git /data/volumes/html
